38e55d5c6aa9f92496a75ee40d849f2127cdc0d1
sagiamda <sagiamda@gmail.com>
2026-05-08 11:41:44 +0900

    fix-24

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_all, Column, String, Float, DateTime, ForeignKey, Integer, create_engine
+from sqlalchemy import Column, String, Float, DateTime, ForeignKey, Integer, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 from datetime import datetime
